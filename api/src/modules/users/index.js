46b5ab7144a117f3c73d8147f9238e798c64bc18
MarlonChi <marlonchiodelli@hotmail.com>
2023-07-18 15:01:15 -0300

    feat: user operations base

--- api/src/modules/users/index.js
+++ api/src/modules/users/index.js
@@ -1,15 +1,55 @@
-export const list = ctx => {
-  ctx.body = 'Helloo!!!';
+import { prisma } from '~/data/index';
+
+export const list = async ctx => {
+  try {
+    const users = await prisma.user.findMany({});
+    ctx.body = users;
+  } catch (err) {
+    console.log(err);
+    ctx.status = 500;
+    ctx.body = 'Ops! Algo deu errado, tente novamente.';
+    return;
+  }
 };
 
-export const create = ctx => {
-  ctx.body = 'Helloo!!!';
+export const create = async ctx => {
+  try {
+    const user = await prisma.user.create({
+      data: ctx.request.body,
+    });
+    ctx.body = user;
+  } catch (err) {
+    ctx.status = 500;
+    ctx.body = 'Ops! Algo deu errado, tente novamente.';
+  }
 };
 
-export const update = ctx => {
-  ctx.body = 'Helloo!!!';
+export const update = async ctx => {
+  const data = {
+    name: ctx.request.body.name,
+    email: ctx.request.body.email,
+    password: ctx.request.body.password,
+  };
+  try {
+    const user = await prisma.user.update({
+      where: { id: ctx.params.id },
+      data,
+    });
+    ctx.body = user;
+  } catch (err) {
+    ctx.status = 500;
+    ctx.body = 'Ops! Algo deu errado, tente novamente.';
+  }
 };
 
-export const remove = ctx => {
-  ctx.body = 'Helloo!!!';
+export const remove = async ctx => {
+  try {
+    await prisma.user.delete({
+      where: { id: ctx.params.id },
+    });
+    ctx.body = { id: ctx.params.id };
+  } catch (err) {
+    ctx.status = 500;
+    ctx.body = 'Ops! Algo deu errado, tente novamente.';
+  }
 };
