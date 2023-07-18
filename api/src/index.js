9eae1b32d1a51c55d702a4a53d86a5fcfe8f252e
MarlonChi <marlonchiodelli@hotmail.com>
2023-07-17 22:29:31 -0300

    feat: scr structure, docker config with postgres and prisma setup

--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,12 @@
-import { x } from './x';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 
-console.log(x(40));
+import { router } from './routes';
+
+const app = new Koa();
+
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(process.env.SERVER_PORT);
---
+++ api/src/modules/users/index.js
@@ -0,0 +1,15 @@
+export const list = ctx => {
+  ctx.body = 'Helloo!!!';
+};
+
+export const create = ctx => {
+  ctx.body = 'Helloo!!!';
+};
+
+export const update = ctx => {
+  ctx.body = 'Helloo!!!';
+};
+
+export const remove = ctx => {
+  ctx.body = 'Helloo!!!';
+};
