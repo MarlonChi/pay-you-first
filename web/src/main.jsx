fe58342b5c264af55016c51511b36638806572cc
MarlonChi <marlonchiodelli@hotmail.com>
2023-08-16 21:54:56 -0300

    feat: global styles

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App.jsx";
 import { ThemeProvider } from "styled-components";
 import { definitions } from "./styles/Theme/definitions.jsx";
 
+import "./styles/global.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={definitions}>
